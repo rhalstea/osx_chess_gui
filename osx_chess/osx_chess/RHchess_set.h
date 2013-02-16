4e4674f1302cbf0fd9fb253459dab373514f1902
Robert Halstead <rhalstea@rhalstea-desktop.local>
2013-02-16 09:34:23 -0800

    added images for the chess pieces

---
+++ osx_chess/osx_chess/RHchess_set.h
@@ -0,0 +1,13 @@
+//
+//  RHchess_set.h
+//  osx_chess
+//
+//  Created by Robert Halstead on 2/16/13.
+//  Copyright (c) 2013 Robert Halstead. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RHchess_set : NSObject
+
+@end
---
+++ osx_chess/osx_chess/RHchess_set.m
@@ -0,0 +1,13 @@
+//
+//  RHchess_set.m
+//  osx_chess
+//
+//  Created by Robert Halstead on 2/16/13.
+//  Copyright (c) 2013 Robert Halstead. All rights reserved.
+//
+
+#import "RHchess_set.h"
+
+@implementation RHchess_set
+
+@end
