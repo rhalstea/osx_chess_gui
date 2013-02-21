5c2214fa985b14a46b967e53a07b0364f70145bd
Robert Halstead <rhalstea@rhalstea-desktop.local>
2013-02-20 22:59:35 -0800

    added the UClicense header to the files

--- osx_chess/osx_chess/RHchess_game.m
+++ osx_chess/osx_chess/RHchess_game.m
@@ -1,11 +1,35 @@
-//
-//  RHchess_game.m
-//  osx_chess
-//
-//  Created by Robert Halstead on 2/20/13.
-//  Copyright (c) 2013 Robert Halstead. All rights reserved.
-//
-
+/*
+    Copyright (c) 2013, Robert J. Halstead
+    All rights reserved.
+ 
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+ 
+    *   Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+ 
+    *   Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in
+        the documentation and/or other materials provided with the
+        distribution.
+ 
+    *   Neither the name of the Robert J. Halstead nor the names of its
+        contributors may be used to endorse or promote products derived
+        from this software without specific prior written permission.
+ 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #import "RHchess_game.h"
 
 @implementation RHchess_game
@@ -24,6 +48,16 @@
     
     return self;
 }
+
+// Modifier Functions
+- (void)set_tile_width:(float)width     { _tile_width = width; }
+- (void)set_tile_height:(float)height   { _tile_height = height; }
+- (void)set_border_bottom:(float)offset { _border_bottom = offset; }
+- (void)set_border_left:(float)offset   { _border_left = offset; }
+
+// Accessor Functions
+- (float)get_board_width    { return (2 * _border_left) + (8.0f * _tile_width); }
+- (float)get_board_height   { return (2 * _border_bottom) + (8.0f * _tile_height); }
 
 - (void)create_board_tiles {
     
